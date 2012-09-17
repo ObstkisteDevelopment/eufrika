1869f469f37b57884bae6df9acb73973e2909e26
Steve Dierker <kiwi@obstkiste.org>
2012-09-17 14:28:30 +0200

    * added template for eufrica but not finished

--- templates/eufrica/index.php
+++ templates/eufrica/index.php
@@ -9,50 +9,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-<jdoc:include type="head" />
-
-<!--[if lte IE 6]>
-<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/ieonly.css" rel="stylesheet" type="text/css" />
-<?php if ($color=="personal") : ?>
-<style type="text/css">
-#line {
-  width:98% ;
-}
-.logoheader {
-  height:200px;
-}
-#header ul.menu {
-  display:block !important;
-  width:98.2% ;
-}
-</style>
-<?php endif; ?>
-<![endif]-->
-
-<!--[if IE 7]>
-<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/ie7only.css" rel="stylesheet" type="text/css" />
-<![endif]-->
-
-<script type="text/javascript">
-  var big ='<?php echo (int)$this->params->get('wrapperLarge');?>%';
-  var small='<?php echo (int)$this->params->get('wrapperSmall'); ?>%';
-  var altopen='<?php echo JText::_('TPL_BEEZ2_ALTOPEN', true); ?>';
-  var altclose='<?php echo JText::_('TPL_BEEZ2_ALTCLOSE', true); ?>';
-  var bildauf='<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/plus.png';
-  var bildzu='<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/minus.png';
-  var rightopen='<?php echo JText::_('TPL_BEEZ2_TEXTRIGHTOPEN', true); ?>';
-  var rightclose='<?php echo JText::_('TPL_BEEZ2_TEXTRIGHTCLOSE', true); ?>';
-  var fontSizeTitle='<?php echo JText::_('TPL_BEEZ2_FONTSIZE', true); ?>';
-  var bigger='<?php echo JText::_('TPL_BEEZ2_BIGGER', true); ?>';
-  var reset='<?php echo JText::_('TPL_BEEZ2_RESET', true); ?>';
-  var smaller='<?php echo JText::_('TPL_BEEZ2_SMALLER', true); ?>';
-  var biggerTitle='<?php echo JText::_('TPL_BEEZ2_INCREASE_SIZE', true); ?>';
-  var resetTitle='<?php echo JText::_('TPL_BEEZ2_REVERT_STYLES_TO_DEFAULT', true); ?>';
-  var smallerTitle='<?php echo JText::_('TPL_BEEZ2_DECREASE_SIZE', true); ?>';
-</script>
-
+  <jdoc:include type="head" /> 
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/eufrika.org/css/template.css" type="text/css" />
 </head>
 <body>
+  <div class="top_border"></div>
+  <div class="container">
+    <nav id="static">
+       <jdoc:include type="modules" name="static-menue" />
+    </nav>
+    <div class="header_container">
+      <header id="eufrika_logo">
+      </header>
+      <div class="left_bar">
+        <nav id="language_selector">
+          <a href="#">
+            <u class="language">F</u>rancais
+          </a> - 
+          <a href="#">
+            <u class="language">E</u>nglish
+          </a>
+        </nav>
+        <br />
+        <div class="social_search_container">
+          <div class="social_networks">
+            FOLLOW US:
+          </div>
+          <br />
+          <div class="searchform">
+            <form>
+              <input type="search" placeholder="Search.." />
+              <input style="display: none;" type="submit" value="search" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav id="mainmenue">
+      <jdoc:include type="modules" name="main-menue" />
+    </nav>
+    <div class="delimiter"></div>
+    <div class="main_frame">
+      <div class="left_frame">
+        <jdoc:include type="modules" name="content"
+      </div>
+      <aside id="sitebar">
+        <jdoc:include type="modules" name="sitebar" />
+      </aside>
+    </div>
 
+    <footer>
+
+    </footer>
+  </div>
 </body>
 </html>
+
