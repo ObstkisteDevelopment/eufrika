6d32670fd7cd5e5fa787cf339bc6f2b71086fa34
Steve Dierker <kiwi@obstkiste.org>
2012-09-24 14:19:36 +0200

    * added styles for language selector * added styles for readmore link

--- templates/eufrica/html/com_content/category/blog_item.php
+++ templates/eufrica/html/com_content/category/blog_item.php
@@ -89,20 +89,21 @@ JHtml::_('behavior.framework');
   endif;
 ?>
     <p class="readmore">
-        <a href="<?php echo $link; ?>">\
-          <?php if (!$params->get('access-view')) :
-            echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE');
-          elseif ($readmore = $this->item->alternative_readmore) :
-            echo $readmore;
-            if ($params->get('show_readmore_title', 0) != 0) :
-                echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
-            endif;
-          elseif ($params->get('show_readmore_title', 0) == 0) :
-            echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
-          else :
-            echo JText::_('COM_CONTENT_READ_MORE');
-            echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
-          endif; ?></a>
+      <img src="<?php echo JURI::root().'/templates/eufrika.org/images/arrows/news_arrow.jpg' ?>" />
+      <a href="<?php echo $link; ?>">
+        <?php if (!$params->get('access-view')) :
+          echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE');
+        elseif ($readmore = $this->item->alternative_readmore) :
+          echo $readmore;
+          if ($params->get('show_readmore_title', 0) != 0) :
+              echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+          endif;
+        elseif ($params->get('show_readmore_title', 0) == 0) :
+          echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
+        else :
+          echo JText::_('COM_CONTENT_READ_MORE');
+          echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+        endif; ?></a>
     </p>
 <?php endif; ?>
 
